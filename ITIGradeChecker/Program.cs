64450096658d6cd3d1cf7922e14658fedc78b251
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-02-25 02:09:08 +0200

    Extract Grades from Page and Print

--- ITIGradeChecker/Program.cs
+++ ITIGradeChecker/Program.cs
@@ -12,14 +12,47 @@ await using var browser = await playwright.Chromium.LaunchAsync(new()
 #endif
 });
 
+Console.Write("Enter your username: ");
+string username = Console.ReadLine()!;
+Console.Write("Enter your password: ");
+string password = Console.ReadLine()!;
+Console.WriteLine();
+
 var page = await browser.NewPageAsync();
 await page.GotoAsync("http://apps.iti.gov.eg/ManagementSystem/intlogin.aspx");
 await page.GetByPlaceholder("Your username").ClickAsync();
-await page.GetByPlaceholder("Your username").FillAsync("YO35619");
+await page.GetByPlaceholder("Your username").FillAsync(username);
 await page.GetByPlaceholder("Password").ClickAsync();
-await page.GetByPlaceholder("Password").FillAsync("iti4");
+await page.GetByPlaceholder("Password").FillAsync(password);
 await page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
 await page.GotoAsync("http://apps.iti.gov.eg/ManagementSystem/Student/StudentGrade.aspx");
-var pageContent = await page.ContentAsync();
 
+var pageContent = await page.ContentAsync();
+var table = page.Locator("#ContentPlaceHolder1_UcViewGrade1_UpdatePanel1 div").Nth(1)
+    .Locator("#ContentPlaceHolder1_UcViewGrade1_GrdViewcourseEval > tbody").Nth(0)
+    .Locator("td");
+
+var tableRows = await table.ElementHandlesAsync();
+
+Dictionary<string, string> grades = new(10);
+string currentCourse = string.Empty;
+
+for (int i = 0; i < tableRows.Count; i++)
+{
+    string textScrapped = await tableRows[i].InnerHTMLAsync();
+
+    if (textScrapped.Length > 1)
+    {
+        grades.Add(textScrapped, string.Empty);
+        currentCourse = textScrapped;
+    }
+    else
+    {
+        grades[currentCourse] = textScrapped;
+        Console.WriteLine($"{currentCourse}: {grades[currentCourse]}");
+    }
+}
+
+Console.WriteLine("\nHave a nice Day...");
+Console.WriteLine("Press Enter to exit...");
 Console.ReadLine();
