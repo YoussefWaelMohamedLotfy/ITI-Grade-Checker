1ddc536a1b0cc0ca5a7a5bf37584731c72385387
Youssef Wael Mohamed-Lotfy <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-02-25 02:26:09 +0200

    Initial Release for Grade Checker (#1)
    * Navigate to Grade Pages
    
    * Extract Grades from Page and Print
    
    * Update README.md
    
    * Added console messages for better experience

--- ITIGradeChecker/Program.cs
+++ ITIGradeChecker/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Playwright;
+
+Console.WriteLine("Hello, ITI !");
+
+using var playwright = await Playwright.CreateAsync();
+
+await using var browser = await playwright.Chromium.LaunchAsync(new()
+{
+    Channel = "msedge",
+#if DEBUG
+    Headless = false
+#endif
+});
+
+Console.Write("Enter your username: ");
+string username = Console.ReadLine()!;
+Console.Write("Enter your password: ");
+string password = Console.ReadLine()!;
+
+var page = await browser.NewPageAsync();
+Console.WriteLine("Logging in...");
+await page.GotoAsync("http://apps.iti.gov.eg/ManagementSystem/intlogin.aspx");
+await page.GetByPlaceholder("Your username").ClickAsync();
+await page.GetByPlaceholder("Your username").FillAsync(username);
+await page.GetByPlaceholder("Password").ClickAsync();
+await page.GetByPlaceholder("Password").FillAsync(password);
+await page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+Console.WriteLine("Logged in...");
+await page.GotoAsync("http://apps.iti.gov.eg/ManagementSystem/Student/StudentGrade.aspx");
+
+Console.WriteLine("Scrapping your grades. Hold on...\n");
+var pageContent = await page.ContentAsync();
+var table = page.Locator("#ContentPlaceHolder1_UcViewGrade1_UpdatePanel1 div").Nth(1)
+    .Locator("#ContentPlaceHolder1_UcViewGrade1_GrdViewcourseEval > tbody").Nth(0)
+    .Locator("td");
+
+var tableRows = await table.ElementHandlesAsync();
+
+Dictionary<string, string> grades = new(10);
+string currentCourse = string.Empty;
+
+for (int i = 0; i < tableRows.Count; i++)
+{
+    string textScrapped = await tableRows[i].InnerHTMLAsync();
+
+    if (textScrapped.Length > 1)
+    {
+        grades.Add(textScrapped, string.Empty);
+        currentCourse = textScrapped;
+    }
+    else
+    {
+        grades[currentCourse] = textScrapped;
+        Console.WriteLine($"{currentCourse}: {grades[currentCourse]}");
+    }
+}
+
+Console.WriteLine("\nHave a nice Day...");
+Console.WriteLine("Press Enter to exit...");
+Console.ReadLine();
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# ITI-Grade-Checker
+# ITI Grade Checker
+
+A scraper that gets your grades for you. No more browsers needed.
+
+## The idea behind this project
+
+This project is a simple Console App that uses [Playwright](https://playwright.dev/dotnet/) to automate the process for you. Playwright does the scrapping while you only wait for your grades to show for you.
+
+Any creative ideas are welcome to be submitted in a new PR.
